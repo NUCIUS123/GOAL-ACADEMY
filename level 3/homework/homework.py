4806e2baf09df2389395a2cfb21f7de68a0f71d9
NUCIUS123 <Ketiekaladze@gmail.com>
2024-05-27 21:28:44 +0400

    level 3

---
+++ level 3/homework/homework.py
@@ -0,0 +1,68 @@
+#დავალება 1
+name = "nuca"
+surname = "natobaidze"
+age = 13
+hobby = "tennis"
+country = "georgia"
+print(name,surname,age,hobby,country)
+
+#დავალება 2
+item = "bottle"
+device = "tv"
+print(item +" " + device)
+
+#დავალება 3
+print(5 + 5)
+print(9 +9)
+print(6+7)
+print(10+10)
+print(11+11)
+print(7+8)
+print(23+33)
+print(8+8)
+print(6+6)
+print(4+4)
+
+print(22-11)
+print(7-3)
+print(9-4)
+print(54-34)
+print(67-54)
+print(80-70)
+print(66-56)
+print(77-66)
+print(567-345)
+print(123-12)
+
+print(77*45)
+print(3*8)
+print(33*82)
+print(23*12)
+print(88*76)
+print(34*2)
+print(44*13)
+print(397*113)
+print(456*765)
+print(56*76)
+
+print(12//6)
+print(9//2)
+print(4//2)
+print(33//2)
+print(28//3)
+print(81//9)
+print(45//5)
+print(40//8)
+print(18//2)
+print(24//3)
+
+print(33**2)
+print(90**33)
+print(28**4)
+print(16**4)
+print(11**1)
+print(45**5)
+print(359**50)
+print(250**150)
+print(56**44)
+print(77**33)
